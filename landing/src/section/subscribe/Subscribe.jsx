844ae60cfedea48c9576b29c54e59a93eaf058ca
ruha02 <ruslan.gzn@gmail.com>
2022-04-29 09:59:41 +0500

    update subscribe section

--- landing/src/section/subscribe/Subscribe.jsx
+++ landing/src/section/subscribe/Subscribe.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
+import StatusAlert, { StatusAlertService } from 'react-status-alert';
 import "./style.css";
+import 'react-status-alert/dist/status-alert.css'
+
 
 export const Subscribe = () => {
-  const [message, setMessage] = useState("")
+  const [alertId, setAlertId] = useState("")
 
   const handleSend = () => {
-    setMessage("Заявка принята. Мы скоро с вами свяжемся")
+    const alert = StatusAlertService.showInfo('Заявка принята. Мы скоро с вами свяжемся')
+    setAlertId(alert)
   }
 
   return (
     <div className="pageSubscribe">
+      <StatusAlert />
       <div className="subscribe">
         <div className="subscribeTitle">Оставить заявку на тестирование</div>
-        <input type='text' placeholder="email" className="subscribeInput"/>
+        <div className="subscribeFormInput" >
+          <input type='text' className="subscribeInput" placeholder="Email"  id='email' name='email' required />
+          <label htmlFor='email' className="subscribeLabel">Email</label>
+        </div>
         <button className="subscribeButton" onClick={handleSend}>Отправить</button>
-        <div className="subscribeMessage">{message}</div>
       </div>
     </div>
   );
