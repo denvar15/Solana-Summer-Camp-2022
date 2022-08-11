fb017464fa7e0d942e69c981030ce739c15237cf
kaladin <maksim.lagus@yandex.ru>
2022-08-11 15:34:02 +0300

    Fix orm mapping

--- backend/src/entities/tradeEntity.ts
+++ backend/src/entities/tradeEntity.ts
@@ -6,28 +6,40 @@ export class Trade {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ name: 'first_nft_address' })
+    @Column({ name: 'first_nft_address', type: 'varchar', array: true })
     firstNFTAddress: string[];
 
-    @Column({ name: 'second_nft_address' })
+    @Column({
+        name: 'second_nft_address',
+        type: 'varchar',
+        array: true,
+    })
     secondNFTAddress: string[];
 
-    @Column({ name: 'first_nft_id' })
+    @Column({ name: 'first_nft_id', type: 'varchar', array: true })
     firstNFTId: number[];
 
-    @Column({ name: 'second_nft_id' })
+    @Column({ name: 'second_nft_id', type: 'varchar', array: true })
     secondNFTId: number[];
 
-    @Column({ name: 'first_solana_mint_address' })
+    @Column({
+        name: 'first_solana_mint_address',
+        type: 'varchar',
+        array: true,
+    })
     firstSolanaMintAddress: string[];
 
-    @Column({ name: 'second_solana_mint_address' })
+    @Column({
+        name: 'second_solana_mint_address',
+        type: 'varchar',
+        array: true,
+    })
     secondSolanaMintAddress: string[];
 
-    @Column({ name: 'first_wrap' })
+    @Column({ name: 'first_wrap', type: 'varchar', array: true })
     firstWrap: string[];
 
-    @Column({ name: 'second_wrap' })
+    @Column({ name: 'second_wrap', type: 'varchar', array: true })
     secondWrap: string[];
 
     @Column({ name: 'first_metadata', type: 'json', array: true })
